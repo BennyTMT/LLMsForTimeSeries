d3ca80c160db069488e80c3bdc7a4db2a632085c
Benny Tan <35400831+BennyTMT@users.noreply.github.com>
2024-05-24 21:30:12 -0500

    Add files via upload

--- LLaTA/data_provider/data_loader.py
+++ LLaTA/data_provider/data_loader.py
@@ -80,7 +80,7 @@ class Dataset_ETT_hour(Dataset):
                                           self.data_path))
 
         border1s = [0, 12 * 30 * 24 - self.seq_len, 12 * 30 * 24 + 4 * 30 * 24 - self.seq_len]
-        border2s = [int(12 * 30 * 24 * self.train_ratio ), 12 * 30 * 24 + 4 * 30 * 24, 12 * 30 * 24 + 8 * 30 * 24]
+        border2s = [12 * 30 * 24 , 12 * 30 * 24 + 4 * 30 * 24, 12 * 30 * 24 + 8 * 30 * 24]
         border1 = border1s[self.set_type]
         border2 = border2s[self.set_type]
 
@@ -117,38 +117,15 @@ class Dataset_ETT_hour(Dataset):
         self.data_stamp = data_stamp
                 
     def __getitem__(self, index):
-        if 'decp_single_linr' in self.model_id:
-            s_begin = index
-            s_end = s_begin + self.seq_len
-            r_begin = s_end - self.label_len
-            r_end = r_begin + self.label_len + self.pred_len
+        s_begin = index
+        s_end = s_begin + self.seq_len
+        r_begin = s_end - self.label_len
+        r_end = r_begin + self.label_len + self.pred_len
 
-            seq_x = self.data_x[s_begin:s_end]
-            seq_y = self.data_y[r_begin:r_end]
-            seq_x_mark = self.data_stamp[s_begin:s_end]
-            seq_y_mark = self.data_stamp[r_begin:r_end]
-            
-            x = torch.tensor(seq_x, dtype=torch.float).transpose(1, 0)  # [7, seq_len]
-            y = torch.tensor(seq_y, dtype=torch.float).transpose(1, 0)  # [7, pred_len]
-            components= []
-            for i in range(self.channel):
-                (trend, seasonal, residual) = decompose(x[i:i+1] , period=self.period)
-                component = torch.concat([trend, seasonal, residual], dim=0)  # [3, seq_len]
-                # print(i , component.shape)
-                components.append(component)
-            components = torch.stack(components) #(7 , 3, seq_len)
-            return components , y ,  seq_x_mark, seq_y_mark
-            # return x , y ,  seq_x_mark, seq_y_mark 
-        else:
-            s_begin = index
-            s_end = s_begin + self.seq_len
-            r_begin = s_end - self.label_len
-            r_end = r_begin + self.label_len + self.pred_len
-
-            seq_x = self.data_x[s_begin:s_end]
-            seq_y = self.data_y[r_begin:r_end]
-            seq_x_mark = self.data_stamp[s_begin:s_end]
-            seq_y_mark = self.data_stamp[r_begin:r_end]
+        seq_x = self.data_x[s_begin:s_end]
+        seq_y = self.data_y[r_begin:r_end]
+        seq_x_mark = self.data_stamp[s_begin:s_end]
+        seq_y_mark = self.data_stamp[r_begin:r_end]
             
         return seq_x, seq_y, seq_x_mark, seq_y_mark
 
@@ -157,7 +134,6 @@ class Dataset_ETT_hour(Dataset):
 
     def inverse_transform(self, data):
         return self.scaler.inverse_transform(data)
-
 
 class Dataset_ETT_minute(Dataset):
     def __init__(self, root_path, flag='train', size=None,
@@ -237,46 +213,24 @@ class Dataset_ETT_minute(Dataset):
         self.data_stamp = data_stamp
 
     def __getitem__(self, index):
-        if 'decp_single_linr' in self.model_id:
-            s_begin = index
-            s_end = s_begin + self.seq_len
-            r_begin = s_end - self.label_len
-            r_end = r_begin + self.label_len + self.pred_len
+        
+        s_begin = index
+        s_end = s_begin + self.seq_len
+        r_begin = s_end - self.label_len
+        r_end = r_begin + self.label_len + self.pred_len
 
-            seq_x = self.data_x[s_begin:s_end]
-            seq_y = self.data_y[r_begin:r_end]
-            seq_x_mark = self.data_stamp[s_begin:s_end]
-            seq_y_mark = self.data_stamp[r_begin:r_end]
-            
-            x = torch.tensor(seq_x, dtype=torch.float).transpose(1, 0)  # [7, seq_len]
-            y = torch.tensor(seq_y, dtype=torch.float).transpose(1, 0)  # [7, pred_len]
-            components= []
-            for i in range(self.channel):
-                (trend, seasonal, residual) = decompose(x[i:i+1] , period=self.period)
-                component = torch.concat([trend, seasonal, residual], dim=0)  # [3, seq_len]
-                # print(i , component.shape)
-                components.append(component)
-            components = torch.stack(components) #(7 , 3, seq_len)
-            return components , y ,  seq_x_mark, seq_y_mark
-        else:
-            s_begin = index
-            s_end = s_begin + self.seq_len
-            r_begin = s_end - self.label_len
-            r_end = r_begin + self.label_len + self.pred_len
-
-            seq_x = self.data_x[s_begin:s_end]
-            seq_y = self.data_y[r_begin:r_end]
-            seq_x_mark = self.data_stamp[s_begin:s_end]
-            seq_y_mark = self.data_stamp[r_begin:r_end]
-            
-            return seq_x, seq_y, seq_x_mark, seq_y_mark
+        seq_x = self.data_x[s_begin:s_end]
+        seq_y = self.data_y[r_begin:r_end]
+        seq_x_mark = self.data_stamp[s_begin:s_end]
+        seq_y_mark = self.data_stamp[r_begin:r_end]
+        
+        return seq_x, seq_y, seq_x_mark, seq_y_mark
 
     def __len__(self):
         return len(self.data_x) - self.seq_len - self.pred_len + 1
 
     def inverse_transform(self, data):
         return self.scaler.inverse_transform(data)
-
 
 class Dataset_Custom(Dataset):
     def __init__(self, root_path, flag='train', size=None,
@@ -307,19 +261,7 @@ class Dataset_Custom(Dataset):
         self.root_path = root_path
         self.data_path = data_path
         
-        # weather  traffic electricity
-        
-        # self.channel self.period
-        if 'weather' in data_path:
-            # per min
-            self.period = 144
-        if 'traffic' in data_path:
-            # per hour 
-            self.period = 24
-        if 'electricity' in data_path:
-            # per hour 
-            self.period = 24
-             
+        print('percent' ,self.percent )
         self.__read_data__()
 
     def __read_data__(self):
@@ -369,61 +311,24 @@ class Dataset_Custom(Dataset):
         elif self.timeenc == 1:
             data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
             data_stamp = data_stamp.transpose(1, 0)
-
+            
         self.data_x = data[border1:border2]
         self.data_y = data[border1:border2]
         self.data_stamp = data_stamp
         
-        # to (18412, 7 , 3 )
-        # if 'decp' in self.model_id:
-        #     print(self.data_x.shape , 'data initialing...')
-        #     # (7  , 18412 )
-        #     self.data_x = torch.tensor(self.data_x, dtype=torch.float).transpose(1, 0) 
-        #     components= []
-        #     for i in range(self.data_x.size(0)):
-        #         (trend, seasonal, residual) = decompose(self.data_x[i:i+1] , period=self.period)
-        #         component = torch.concat([trend, seasonal, residual], dim=0)  # [3, seq_len]
-        #         # print(i , component.shape)
-        #         components.append(component)
-        #     components = torch.stack(components) #(7 , 3, 18412)
-        #     self.data_x = components.transpose(2, 0, 1)
-        #     #  #(seq_len  , 7 , 3, )
-        #     print(self.data_x.shape)
             
     def __getitem__(self, index):
-        if 'decp_single_linr' in self.model_id:
-            s_begin = index
-            s_end = s_begin + self.seq_len
-            r_begin = s_end - self.label_len
-            r_end = r_begin + self.label_len + self.pred_len
+        s_begin = index
+        s_end = s_begin + self.seq_len
+        r_begin = s_end - self.label_len
+        r_end = r_begin + self.label_len + self.pred_len
 
-            seq_x = self.data_x[s_begin:s_end]
-            seq_y = self.data_y[r_begin:r_end]
-            seq_x_mark = self.data_stamp[s_begin:s_end]
-            seq_y_mark = self.data_stamp[r_begin:r_end]
-            
-            x = torch.tensor(seq_x, dtype=torch.float).transpose(1, 0)  # [7, 3, seq_len]
-            y = torch.tensor(seq_y, dtype=torch.float).transpose(1, 0)  # [7, pred_len]
-            components= []
-            for i in range(x.size(0)):
-                (trend, seasonal, residual) = decompose(x[i:i+1] , period=self.period)
-                component = torch.concat([trend, seasonal, residual], dim=0)  # [3, seq_len]
-                # print(i , component.shape)
-                components.append(component)
-            components = torch.stack(components) #(7 , 3, seq_len)
-            return components , y ,  seq_x_mark, seq_y_mark
-        else:
-            s_begin = index
-            s_end = s_begin + self.seq_len
-            r_begin = s_end - self.label_len
-            r_end = r_begin + self.label_len + self.pred_len
+        seq_x = self.data_x[s_begin:s_end]
+        seq_y = self.data_y[r_begin:r_end]
+        seq_x_mark = self.data_stamp[s_begin:s_end]
+        seq_y_mark = self.data_stamp[r_begin:r_end]
 
-            seq_x = self.data_x[s_begin:s_end]
-            seq_y = self.data_y[r_begin:r_end]
-            seq_x_mark = self.data_stamp[s_begin:s_end]
-            seq_y_mark = self.data_stamp[r_begin:r_end]
-
-            return seq_x, seq_y, seq_x_mark, seq_y_mark
+        return seq_x, seq_y, seq_x_mark, seq_y_mark
 
     def __len__(self):
         return len(self.data_x) - self.seq_len - self.pred_len + 1
